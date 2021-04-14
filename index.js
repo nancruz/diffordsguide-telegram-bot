08145ab960fac4525b4621c5e0de83cb6fddc8ae
Nuno Cruz <cruz.nuno7@gmail.com>
2021-04-14 19:47:40 +0100

    Fix import

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Telegraf = require('telegraf');
+const { Telegraf } = require('telegraf');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const diffordsguideHost = 'https://www.diffordsguide.com';
